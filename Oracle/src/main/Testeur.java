76e0461a688be7c68409f6a8c5b7f6dd1d90a2ef
steven <steven.pescheteau@gmail.com>
2015-11-13 16:50:52 +0100

    Ajout d'un diagramme de classe

--- Oracle/src/main/Testeur.java
+++ Oracle/src/main/Testeur.java
@@ -4,13 +4,7 @@ import java.util.List;
 
 import tests.AbstractTest;
 
-
-
 public class Testeur {
-
-	/**
-	 * @param args
-	 */
 
 	public static final int HELP = 0;
 	public static final int COUNT = 1;
@@ -19,22 +13,32 @@ public class Testeur {
 	private int option;
 	private List<AbstractTest> tests;
 	
-	// No option
+	/**
+	 * Testeur, sans option. Dispose de plusieurs fonctions de tests pour 
+	 */
 	public Testeur(){
 		option = -1;
 		tests = new ArrayList<AbstractTest>();
 	}
 	
+	/**
+	 * 
+	 * @param option
+	 */
 	public Testeur(int option){
 		this.option = option;
 		tests = new ArrayList<AbstractTest>();
 	}
-	
+
 	// Application des tests
+	public void testerPrettyPrinter(){
+		tests.clear();
+		//TODO
+	}
+	
 	
 	public static void main(String[] args) {
-		
-	//	Testeur testeur = new Testeur(Testeur.COUNT);
-		
+		Testeur testeur = new Testeur(Testeur.COUNT);
+		testeur.testerPrettyPrinter();
 	}
 }
