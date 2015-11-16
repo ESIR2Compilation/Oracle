1a9e1405aa65dfb2b6dfd495a3f7022774c24ec0
steven <steven.pescheteau@gmail.com>
2015-11-16 11:27:40 +0100

    Ajout de fichiers d'oracle, amélioration du testeur : il récupère les fichiers dans le dossier entries.

--- Oracle/src/main/Testeur.java
+++ Oracle/src/main/Testeur.java
@@ -1,5 +1,6 @@
 package main;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import tests.AbstractTest;
@@ -12,22 +13,27 @@ public class Testeur {
 	
 	private int option;
 	private List<AbstractTest> tests;
+	private List<String> files;
 	
 	/**
-	 * Testeur, sans option. Dispose de plusieurs fonctions de tests pour 
+	 * Testeur, sans option.
 	 */
 	public Testeur(){
-		option = -1;
-		tests = new ArrayList<AbstractTest>();
+		this(-1);
 	}
 	
 	/**
-	 * 
-	 * @param option
+	 * Testeur avec options;
+	 * Testeur.COUNT : Compte le nombre de tests échoués
+	 * Testeur.DETAILED : Détaille tous les tests, réussis ou échoués
+	 * Testeur.HELP  : MAN du Testeur
+	 * @param option Testeur. [COUNT, DETAILED, HELP]
 	 */
 	public Testeur(int option){
 		this.option = option;
 		tests = new ArrayList<AbstractTest>();
+		files = Arrays.asList(new java.io.File("./src/entries").list());
+		System.out.println(files);
 	}
 
 	// Application des tests
@@ -35,7 +41,7 @@ public class Testeur {
 		tests.clear();
 		//TODO
 	}
-	
+
 	
 	public static void main(String[] args) {
 		Testeur testeur = new Testeur(Testeur.COUNT);
